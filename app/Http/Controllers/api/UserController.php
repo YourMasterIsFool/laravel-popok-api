2851b9a0a1d26dd7e18977106884fc3369c5d2e6
Sosuke Aizen <verrandyb@gmail.com>
2023-10-25 01:01:20 +0700

    adjustment current user

--- app/Http/Controllers/api/UserController.php
+++ app/Http/Controllers/api/UserController.php
@@ -25,7 +25,7 @@ class UserController extends Controller
 
   
 
-        return response()->ok(auth()->user());
+        return response()->ok($findUser);
     }
 
     public function index(Request $request) {
